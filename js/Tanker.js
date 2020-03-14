e1dcda1ce78ed409599bf719a973279aecbcb190
ekanshgupta0202 <ekanshgupta0202@outlook.com>
2020-03-14 17:40:29 +0530

    hi

--- js/Tanker.js
+++ js/Tanker.js
@@ -1,11 +1,35 @@
 class Tanker {
-  constructor(x, y, width, height) {
+  constructor(x,y,width,height) {
       var options = {
       isStatic: true
      }
+     this.body = Bodies.rectangle(x,y,width,height,options);
+     this.width = width;
+     this.height = height;
+     World.add(world,this.body);
     };
 
     display(){
+      var pos = this.body.position;
+      var angle = this.body.angle;
+
+      if(keyIsDown(LEFT_ARROW) && angle > -0.8708){
+        angle -=0.05
+        Matter.Body.setAngle( tanker.body, angle);
+      }
+      if(keyIsDown(RIGHT_ARROW) && angle < 0){
+        angle +=0.05
+        Matter.Body.setAngle( tanker.body, angle);
+      }
+      
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      //rectMode(CENTER);
+      rect(0,0,this.width,this.height);
+      pop();
+      arc(60, 560, 90, 100, PI , TWO_PI);
+      rect(50,570,150,20);
       // Draw the tanker the way you like.
       // You could also use an image if you want a specific look
 
--- sketch.js
+++ sketch.js
@@ -2,7 +2,8 @@
 
 const { Engine, World, Bodies, Body, Mouse, MouseConstraint, Constraint, Composite, Detector } = Matter;
 // The above line creates different constant variables for Engine, World, Bodies etc.
-
+var engine, world;
+var ground;
 /*
 
 const {Engine} = Matter 
@@ -12,11 +13,20 @@ onst Engine = Matter.Engine
 */
 
 function setup() {
+    createCanvas(600,600);
+    engine = Engine.create();
+    world = engine.world;
+    ground = new Ground(400,580,800,05);
+    tanker = new Tanker(105, 525, 90, 20);    
     // Setup the canvas, the ground the, tanker, the shooting ball and the bubble balls.
 
 }
 
 function draw() {
+    background("black");
+    Engine.update(engine);
+    ground.display();
+    tanker.display();
 // Remember to update the Matter Engine and set the background.
 }
 
